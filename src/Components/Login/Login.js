e0541058f5677d949dd4f71fab9776895daba53c
MCVNick <mcvnick@gmail.com>
2019-03-18 15:02:13 -0600

    added the return

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,16 +2,18 @@ import React, {Component} from 'react'
 
 class Login extends Component {
     render() {
-        <div className='Login-Component-Parent'>
-            <ul>
-                {}
-            </ul>
-            <h1>Login</h1>
-            <input placeholder='Email'/>
-            <input placeholder='Password'/>
-            <button>Login</button>
-            <p>Don't have an account <span>create one</span></p>
-        </div>
+        return (
+            <div className='Login-Component-Parent'>
+                <ul>
+                    {}
+                </ul>
+                <h1>Login</h1>
+                <input placeholder='Email'/>
+                <input placeholder='Password'/>
+                <button>Login</button>
+                <p>Don't have an account <span>create one</span></p>
+            </div>
+        )
     }
 }
 
