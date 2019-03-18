c88550ed16ae3a838e89e366c2fc1390dee33485
MCVNick <mcvnick@gmail.com>
2019-03-18 14:51:53 -0600

    fixed the Login compoenent

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,18 +2,16 @@ import React, {Component} from 'react'
 
 class Login extends Component {
     render() {
-        render (
-            <div className='Login-Component-Parent'>
-                <ul>
-                    {}
-                </ul>
-                <h1>Login</h1>
-                <input placeholder='Email'/>
-                <input placeholder='Password'/>
-                <button>Login</button>
-                <p>Don't have an account <span>create one</span></p>
-            </div>
-        )
+        <div className='Login-Component-Parent'>
+            <ul>
+                {}
+            </ul>
+            <h1>Login</h1>
+            <input placeholder='Email'/>
+            <input placeholder='Password'/>
+            <button>Login</button>
+            <p>Don't have an account <span>create one</span></p>
+        </div>
     }
 }
 
