46c5c428efb7868b0788465ceeccd5a536589e66
David Wells <davidwells@Davids-MacBook-Pro.local>
2019-03-28 14:23:23 -0600

    restructred projects component

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
-import Boxes from './../Boxes/Boxes';
+// import { Link} from 'react-router-dom';
+// import Boxes from './../Boxes/Boxes';
+import Editor from './Editor/Editor';
+import FileTree from './FileTree/FileTree';
 
 class Projects extends Component {
   state = {
@@ -15,12 +17,13 @@ class Projects extends Component {
   }
 
   render() {
-    const mappedFiles = this.state.files.map((filesObj, i) => {
-      return <Link to = {`/Editor/${filesObj.id}`}><Boxes key = {i} id = {filesObj.id} name = {filesObj.file_name} /></Link>
-    })
+    // const mappedFiles = this.state.files.map((filesObj, i) => {
+    //   return <Link to = {`/Editor/${filesObj.id}`}><Boxes key = {i} id = {filesObj.id} name = {filesObj.file_name} /></Link>
+    // })
     return(
-      <div>
-        {mappedFiles}
+      <div className='editor_page'>
+        <FileTree/>
+        <Editor/>
       </div>
     )
   }
